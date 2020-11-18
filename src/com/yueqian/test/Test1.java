6e970fe9204546b8f8c81de2bd28e40e074d2eb9
loveJavaisliu <74598162+loveJavaisliu@users.noreply.github.com>
2020-11-18 07:11:10 +0000

    Update Test1.java

--- src/com/yueqian/test/Test1.java
+++ src/com/yueqian/test/Test1.java
@@ -4,4 +4,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    
+    public void fun(){
+        System.out.println("方法一!");
+    }
 }
